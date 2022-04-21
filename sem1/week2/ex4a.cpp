c12190934fa0850f17af1f74d61c9db283f3bd58
guto <91899995+gutoso@users.noreply.github.com>
2022-04-20 23:41:25 -0300

    Update ex4a.cpp

--- sem1/week2/ex4a.cpp
+++ sem1/week2/ex4a.cpp
@@ -14,14 +14,14 @@ using namespace std;
 
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "Portuguese");
-    int opc = 1, contador = 0, maior = 0;
+    int opc = 1, contador = 0, maior = 0; // Primeiro iniciamos nossas variáveis! A variável 'opc' não pode ser 0, pois senão jamais entrará no laço!
     
     cout << "==============================" << endl;
     while (opc != 0){
         cout << "Digite um número (0 para parar): ";
         cin >> opc;
-        contador++;
-        if (opc > maior){
+        contador++; // Basicamente este contador aumenta de um em um para mostrar a quantidade de vezes que o laço foi executado, ou seja, a quantidade de números digitados!
+        if (opc > maior){ // Aqui fazemos uma comparação para saber qual o maior número digitado. Se ele for maior que o anterior armazenado, então ele será armazenado!
             maior = opc;
         }
     }
