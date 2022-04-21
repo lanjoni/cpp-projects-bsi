1289a4af807dab6ceb902856642ff16e3c1cf583
guto <91899995+gutoso@users.noreply.github.com>
2022-04-21 14:01:18 -0300

    Update ex3a.cpp

--- sem1/week2/ex3a.cpp
+++ sem1/week2/ex3a.cpp
@@ -28,26 +28,34 @@ int main(int argc, char** argv){
     cout << "- - Subtrair;" << endl;
     cout << "/ - Dividir;" << endl;
     cout << "* - Multiplicar;" << endl;
+    cout << "S - Sair;" << endl;
     cin >> opc;
-    cout << "\nInforme o primeiro número: ";
-    cin >> n1;
-    cout << "\nInforme o segundo número: ";
-    cin >> n2;
 
-    switch (opc){ // A única diferença deste 'switch' é: ele verifica caracteres! Sim, de modo geral! Para trabalhar com letras usamos 'toupper' pois todas as letras se tornam maiúsculas!
-        case '+':
-            cout << "O resultado de " << n1 << " + " << n2 << " é " << n1 + n2 << endl;
-            break;
-        case '-':
-            cout << "O resultado de " << n1 << " - " << n2 << " é " << n1 - n2 << endl;
-            break;
-        case '/':
-            cout << "O resultado de " << n1 << " / " << n2 << " é " << n1 / n2 << endl;
-            break;
-        case '*':
-            cout << "O resultado de " << n1 << " * " << n2 << " é " << n1 * n2 << endl;
-            break;
-        default:
-            cout << "Opção inválida!" << endl;
+    opc = toupper(opc);
+
+    if (opc == 'S'){
+        exit(0);
+    } else {
+        cout << "\nInforme o primeiro número: ";
+        cin >> n1;
+        cout << "\nInforme o segundo número: ";
+        cin >> n2;
+            
+        switch (opc){
+            case '+':
+                cout << "O resultado de " << n1 << " + " << n2 << " é " << n1 + n2 << endl;
+                break;
+            case '-':
+                cout << "O resultado de " << n1 << " - " << n2 << " é " << n1 - n2 << endl;
+                break;
+            case '/':
+                cout << "O resultado de " << n1 << " / " << n2 << " é " << n1 / n2 << endl;
+                break;
+            case '*':
+                cout << "O resultado de " << n1 << " * " << n2 << " é " << n1 * n2 << endl;
+                break;
+            default:
+                cout << "Opção inválida!" << endl;
+        }
     }
 }
