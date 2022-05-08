9d1d41890e4a67296127949ce7e285560bc5b5c0
guto <joaoaugustolanjoni@gmail.com>
2022-05-08 10:05:09 -0300

    Update

--- sem1/week3/Ex1_2604/cmake-build-debug/cmake_install.cmake
+++ sem1/week3/Ex1_2604/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/guto/Documents/programming/c++/Ex1_2604
+# Install script for directory: /Users/guto/Documents/GitHub/cpp-projects-bsi/sem1/week3/Ex1_2604
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/guto/Documents/programming/c++/Ex1_2604/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/guto/Documents/GitHub/cpp-projects-bsi/sem1/week3/Ex1_2604/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
